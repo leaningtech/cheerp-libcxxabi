02cfbc5c503d80631def6a938b4bf2a9106c6ffc
Alessandro Pignotti <a.pignotti@sssup.it>
2013-09-04 18:23:21 +0200

    Use 32bit guards on duetto

--- include/cxxabi.h
+++ include/cxxabi.h
@@ -71,7 +71,7 @@ extern LIBCXXABI_NORETURN void __cxa_pure_virtual(void);
 extern LIBCXXABI_NORETURN void __cxa_deleted_virtual(void);
 
 // 3.3.2 One-time Construction API
-#ifdef __arm__
+#if defined(__arm__) || defined(__DUETTO__)
 extern int  __cxa_guard_acquire(uint32_t*);
 extern void __cxa_guard_release(uint32_t*);
 extern void __cxa_guard_abort(uint32_t*);
