8542a4f0a1df1c4cd6d684b44e03b6c9b93c4279
Alessandro Pignotti <a.pignotti@sssup.it>
2014-06-06 16:40:44 +0200

    Rebranding to Cheerp

--- src/cxa_guard.cpp
+++ src/cxa_guard.cpp
@@ -48,7 +48,7 @@ namespace __cxxabiv1
 namespace
 {
 
-#if defined(__arm__) || defined(__DUETTO__)
+#if defined(__arm__) || defined(__CHEERP__)
 // A 32-bit, 4-byte-aligned static data value. The least significant 2 bits must
 // be statically initialized to 0.
 typedef uint32_t guard_type;
@@ -88,13 +88,13 @@ std::__libcpp_mutex_t guard_mut = _LIBCPP_MUTEX_INITIALIZER;
 std::__libcpp_condvar_t guard_cv = _LIBCPP_CONDVAR_INITIALIZER;
 #endif
 
-#if (defined(__APPLE__) || defined(__DUETTO__)) && !defined(__arm__)
+#if (defined(__APPLE__) || defined(__CHEERP__)) && !defined(__arm__)
 
 typedef uint32_t lock_type;
 
 #if __LITTLE_ENDIAN__
 
-#ifdef __DUETTO__
+#ifdef __CHEERP__
 inline
 lock_type
 get_lock(uint32_t x)
@@ -108,7 +108,7 @@ set_lock(uint32_t& x, lock_type y)
 {
     x = static_cast<uint32_t>(y);
 }
-#else // __DUETTO__
+#else // __CHEERP__
 inline
 lock_type
 get_lock(uint64_t x)
@@ -122,7 +122,7 @@ set_lock(uint64_t& x, lock_type y)
 {
     x = static_cast<uint64_t>(y) << 32;
 }
-#endif // __DUETTO__
+#endif // __CHEERP__
 
 #else  // __LITTLE_ENDIAN__
 
@@ -205,7 +205,7 @@ extern "C"
 #ifndef _LIBCXXABI_HAS_NO_THREADS
 _LIBCXXABI_FUNC_VIS int __cxa_guard_acquire(guard_type *guard_object) {
     char* initialized = (char*)guard_object;
-#ifndef __DUETTO__
+#ifndef __CHEERP__
     if (std::__libcpp_mutex_lock(&guard_mut))
         abort_message("__cxa_guard_acquire failed to acquire mutex");
 #endif
@@ -245,7 +245,7 @@ _LIBCXXABI_FUNC_VIS int __cxa_guard_acquire(guard_type *guard_object) {
         else
             set_lock(*guard_object, id);
 #else  // !__APPLE__ || __arm__
-#ifndef __DUETTO__
+#ifndef __CHEERP__
         while (get_lock(*guard_object))
             if (std::__libcpp_condvar_wait(&guard_cv, &guard_mut))
                 abort_message("__cxa_guard_acquire condition variable wait failed");
@@ -255,7 +255,7 @@ _LIBCXXABI_FUNC_VIS int __cxa_guard_acquire(guard_type *guard_object) {
             set_lock(*guard_object, true);
 #endif  // !__APPLE__ || __arm__
     }
-#ifndef __DUETTO__
+#ifndef __CHEERP__
     if (std::__libcpp_mutex_unlock(&guard_mut))
         abort_message("__cxa_guard_acquire failed to release mutex");
 #endif
@@ -263,13 +263,13 @@ _LIBCXXABI_FUNC_VIS int __cxa_guard_acquire(guard_type *guard_object) {
 }
 
 _LIBCXXABI_FUNC_VIS void __cxa_guard_release(guard_type *guard_object) {
-#ifndef __DUETTO__
+#ifndef __CHEERP__
     if (std::__libcpp_mutex_lock(&guard_mut))
         abort_message("__cxa_guard_release failed to acquire mutex");
 #endif
     *guard_object = 0;
     set_initialized(guard_object);
-#ifndef __DUETTO__
+#ifndef __CHEERP__
     if (std::__libcpp_mutex_unlock(&guard_mut))
         abort_message("__cxa_guard_release failed to release mutex");
     if (std::__libcpp_condvar_broadcast(&guard_cv))
@@ -278,12 +278,12 @@ _LIBCXXABI_FUNC_VIS void __cxa_guard_release(guard_type *guard_object) {
 }
 
 _LIBCXXABI_FUNC_VIS void __cxa_guard_abort(guard_type *guard_object) {
-#ifndef __DUETTO__
+#ifndef __CHEERP__
     if (std::__libcpp_mutex_lock(&guard_mut))
         abort_message("__cxa_guard_abort failed to acquire mutex");
 #endif
     *guard_object = 0;
-#ifndef __DUETTO__
+#ifndef __CHEERP__
     if (std::__libcpp_mutex_unlock(&guard_mut))
         abort_message("__cxa_guard_abort failed to release mutex");
     if (std::__libcpp_condvar_broadcast(&guard_cv))
